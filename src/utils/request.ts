443aa7c5dbabab450a85520848bd161373616b0e
MasonWang <bigmasonwang@gmail.com>
2021-11-24 12:50:13 +1030

    fix: remove uat

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,5 +1,5 @@
 import axios, { AxiosRequestConfig } from "axios";
-import { DEVELOPMENT_API_URL, PRODUCTION_API_URL, UAT_API_URL } from "../constants/apiUrl";
+import { DEVELOPMENT_API_URL, PRODUCTION_API_URL } from "../constants/apiUrl";
 
 const getApiUrl = () => {
   const environment = process.env.NODE_ENV;
@@ -7,8 +7,6 @@ const getApiUrl = () => {
   switch (environment) {
     case 'production':
       return PRODUCTION_API_URL;
-    case 'uat':
-      return UAT_API_URL;
     default:
       return DEVELOPMENT_API_URL;
   }
