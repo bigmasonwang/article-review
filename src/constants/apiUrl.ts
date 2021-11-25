ae3fc49f6e7afce8a60e77fdf6a8f90f92522a81
MasonWang <bigmasonwang@gmail.com>
2021-11-25 20:35:14 +1030

    feat: connect server

--- src/constants/apiUrl.ts
+++ src/constants/apiUrl.ts
@@ -1,2 +1,2 @@
 export const DEVELOPMENT_API_URL = 'http://localhost:5000';
-export const PRODUCTION_API_URL = 'http://54.252.200.207:5000/';
+export const PRODUCTION_API_URL = 'http://54.252.200.207:5000';
